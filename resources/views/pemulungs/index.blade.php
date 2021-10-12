5d85d558e04a2b95ab79ffc04a49635cc8f1ce66
metalx-yan <farandibagas@gmail.com>
2021-10-12 19:06:45 +0700

    Last Updated

--- resources/views/pemulungs/index.blade.php
+++ resources/views/pemulungs/index.blade.php
@@ -84,11 +84,20 @@
 
 @section('scripts')
     <script src="https://cdn.datatables.net/1.10.19/js/jquery.dataTables.min.js"></script>
+    <script src="https://cdn.datatables.net/buttons/2.0.1/js/dataTables.buttons.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jszip/3.1.3/jszip.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.53/pdfmake.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.53/vfs_fonts.js"></script>
+    <script src="https://cdn.datatables.net/buttons/2.0.1/js/buttons.html5.min.js"></script>
+    <script src="https://cdn.datatables.net/buttons/2.0.1/js/buttons.print.min.js"></script>
 
     <script>
         $(document).ready( function () {
             $('#myTable').DataTable({
-                "scrollX" : true
+                dom: 'Bfrtip',
+                buttons: [
+                   'excel', 'pdf', 'print'
+                ]
             });
         } );
     </script>
